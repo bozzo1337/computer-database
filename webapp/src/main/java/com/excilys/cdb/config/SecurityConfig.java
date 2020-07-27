91ac5cdc50141a13a38c2c1abc953f7cb28ebd13
bozzo1337 <benjamin.longa@gmail.com>
2020-07-27 09:44:46 +0200

    v2.02

---
+++ webapp/src/main/java/com/excilys/cdb/config/SecurityConfig.java
@@ -0,0 +1,73 @@
+package com.excilys.cdb.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.web.authentication.www.DigestAuthenticationEntryPoint;
+import org.springframework.security.web.authentication.www.DigestAuthenticationFilter;
+
+import com.excilys.cdb.connector.MyDataSource;
+
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Autowired
+	private MyDataSource dataSource;
+	
+	@Bean
+	public DigestAuthenticationFilter digestAuthenticationFilter() throws Exception {
+		DigestAuthenticationFilter digestAuthenticationFilter = new DigestAuthenticationFilter();
+		digestAuthenticationFilter.setUserDetailsService(userDetailsServiceBean());
+		digestAuthenticationFilter.setAuthenticationEntryPoint(digestEntryPoint());
+		digestAuthenticationFilter.setPasswordAlreadyEncoded(false);
+		return digestAuthenticationFilter;
+	}
+	
+	@Bean
+	@Override
+	public UserDetailsService userDetailsServiceBean() throws Exception {
+		JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
+		jdbcUserDetailsManager.deleteUser("user");
+		jdbcUserDetailsManager.createUser(User.builder().username("user").password("user").disabled(false).authorities("ROLE_USER").build());
+		return jdbcUserDetailsManager;
+	}
+	
+	@Bean
+	public DigestAuthenticationEntryPoint digestEntryPoint() {
+		DigestAuthenticationEntryPoint entryPoint = new DigestAuthenticationEntryPoint();
+		entryPoint.setRealmName("CDB Realm");
+		entryPoint.setKey("quarante-deux");
+		return entryPoint;
+	}
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(userDetailsServiceBean());
+	}
+
+	@Bean
+	public Pbkdf2PasswordEncoder passwordEncoder() {
+		return new Pbkdf2PasswordEncoder();
+	}
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.addFilter(digestAuthenticationFilter())
+		.exceptionHandling().authenticationEntryPoint(digestEntryPoint())
+		.and()
+		.authorizeRequests().antMatchers("/create*").authenticated();
+		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+		http.formLogin().disable();
+	}
+}
